f421a61a365dbf05a2217c439b2c76a19230e959
guel- <abdullah-guel@gmx.de>
2025-12-29 15:39:12 +0100

    Initial commit for Spring boot project setup

---
+++ src/test/java/com/abdullah/misar/MisarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abdullah.misar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MisarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
